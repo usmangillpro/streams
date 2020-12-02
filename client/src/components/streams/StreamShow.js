bf64090996efcaccbaec5436a1c488447c293013
usmangillpro <usmangillpro@gmail.com>
2020-12-02 19:45:35 +0500

    (add): StreamShow component is now functional

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../redux/actions";
 
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+class StreamShow extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  render() {
+    const { title, description } = this.props.stream;
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div>
+        <h1>{title}</h1>
+        <h5>{description}</h5>
+      </div>
+    );
+  }
+}
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id],
+  };
 };
-
-export default StreamShow;
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
