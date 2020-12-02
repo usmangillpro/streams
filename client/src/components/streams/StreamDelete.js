affb3cdb9dd2cc57fdaef1a91b36f15fc2b85134
usmangillpro <usmangillpro@gmail.com>
2020-12-02 14:55:43 +0500

    (add): StreamDelete is now functional | Modal added

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,13 +1,61 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import Modal from "../Modal";
+import history from "../../history";
+import { fetchStream, deleteStream } from "../../redux/actions";
 
-const StreamDelete = () => {
-  return (
-    <div>
-      StreamDelete
-      <Modal />
-    </div>
-  );
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  actions = () => {
+    const { id } = this.props.match.params;
+    return (
+      <React.Fragment>
+        <button
+          onClick={() => this.props.deleteStream(id)}
+          className="ui primary button"
+        >
+          Delete
+        </button>
+        <Link to="/" className="ui button">
+          Cancel
+        </Link>
+      </React.Fragment>
+    );
+  };
+
+  renderContent() {
+    if (!this.props.stream) {
+      return "Loading";
+    }
+    return (
+      <div>
+        Sure to delete <b>{this.props.stream.title}</b> ?
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <Modal
+        header="Delete Stream"
+        content={this.renderContent()}
+        actions={this.actions()}
+        onDismiss={() => history.push("/")}
+      />
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id],
+  };
 };
 
-export default StreamDelete;
+export default connect(mapStateToProps, { fetchStream, deleteStream })(
+  StreamDelete
+);
--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -61,7 +61,8 @@ export const editStream = (id, formValues) => async (dispatch) => {
 };
 
 export const deleteStream = (id) => async (dispatch) => {
-  streams.delete(`/streams/${id}`);
+  await streams.delete(`/streams/${id}`);
 
   dispatch({ type: DELETE_STREAM, payload: id });
+  history.push("/");
 };
