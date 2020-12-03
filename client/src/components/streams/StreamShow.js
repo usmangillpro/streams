81bff0c5e563df7f12a3475fd1d076b38229f11d
usmangillpro <usmangillpro@gmail.com>
2020-12-03 15:25:20 +0500

    (add): Added video support in StreamShow

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -9,15 +9,37 @@ class StreamShow extends React.Component {
 
     this.videoRef = React.createRef();
   }
-
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+    const { id } = this.props.match.params;
+
+    this.props.fetchStream(id);
+
+    this.buildPlayer();
   }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer() {
+    const { id } = this.props.match.params;
+
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  }
+
   render() {
-    const { title, description } = this.props.stream;
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
+    const { title, description } = this.props.stream;
     return (
       <div>
         <video ref={this.videoRef} style={{ width: "100%" }} controls />
